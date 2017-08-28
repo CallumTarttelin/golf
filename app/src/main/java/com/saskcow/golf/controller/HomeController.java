a46457a4b23b750e645912c1d42fd4b7d4cb84ed
Chris Tarttelin <chris@tarttelin.co.uk>
2017-08-28 11:08:38 +0100

    Outline of golf repo

---
+++ app/src/main/java/com/saskcow/golf/controller/HomeController.java
@@ -0,0 +1,18 @@
+package com.saskcow.golf.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String index(Model model) {
+        model.addAttribute("greeting", "You'll love our shiny courses");
+        return "index";
+
+    }
+}
---
+++ app/src/main/resources/templates/index.ftl
@@ -0,0 +1,7 @@
+<html>
+<head><title>My Lovely Golfing App</title></head>
+<body>
+<h3>What a wonderful golfing application!</h3>
+Aren't I lovely! ${greeting}
+</body>
+</html>
